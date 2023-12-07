17aaee31938f13af89ab202cb0b4c4a6f1e449af
sharcoder@gmail.com <sharcoder@gmail.com>
2023-12-07 17:55:53 -0600

    Secured connection strings - added azure key vault

--- EmployeeScreeningTest/Program.cs
+++ EmployeeScreeningTest/Program.cs
@@ -1,26 +1,50 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Azure.Cosmos;
+using Azure.Identity;
 using EmployeeScreeningTest.Services.Interfaces;
 using EmployeeScreeningTest.Services.Implementations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
+using Microsoft.Extensions.Caching.Distributed;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add configuration sources (appsettings.json, appsettings.Development.json, etc.).
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+// Add Azure Key Vault using the new method
+var keyVaultUrl = new Uri($"https://{builder.Configuration["KeyVaultName"]}.vault.azure.net/");
+builder.Configuration.AddAzureKeyVault(keyVaultUrl, new DefaultAzureCredential());
 
 // Add services to the container.
 builder.Services.AddControllers();
 
+// Register the Redis Cache client
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration["RedisCacheConnectionString"];
+});
+
 // Register the Cosmos DB client and EmployeeRepository service
-builder.Services.AddSingleton<CosmosClient>(sp => new CosmosClient(builder.Configuration["CosmosDb:ConnectionString"]));
+builder.Services.AddSingleton<CosmosClient>(sp =>
+{
+    var connectionString = builder.Configuration["CosmosDbConnectionString"];
+    return new CosmosClient(connectionString);
+});
 builder.Services.AddSingleton<IEmployeeRepository, EmployeeRepository>(sp =>
 {
     var cosmosClient = sp.GetRequiredService<CosmosClient>();
-    string databaseName = builder.Configuration["CosmosDb:DatabaseName"];
-    string containerName = builder.Configuration["CosmosDb:ContainerName"];
-    return new EmployeeRepository(cosmosClient, databaseName, containerName);
+    var cache = sp.GetRequiredService<IDistributedCache>();
+    string databaseName = builder.Configuration["CosmosDatabaseName"];
+    string containerName = builder.Configuration["CosmosContainerName"];
+    return new EmployeeRepository(cosmosClient, databaseName, containerName, cache);
 });
+
 
 // Register the EmployeeService service
 builder.Services.AddScoped<IEmployeeService, EmployeeService>();
-
-// TODO: Register Redis Cache client and related services when ready
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
--- EmployeeScreeningTest/Services/Implementations/EmployeeRepository.cs
+++ EmployeeScreeningTest/Services/Implementations/EmployeeRepository.cs
@@ -1,7 +1,11 @@
 ﻿using EmployeeScreeningTest.Models;
 using EmployeeScreeningTest.Services.Interfaces;
 using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json; // Add this to handle JSON serialization
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace EmployeeScreeningTest.Services.Implementations
@@ -9,51 +13,104 @@ namespace EmployeeScreeningTest.Services.Implementations
     public class EmployeeRepository : IEmployeeRepository
     {
         private readonly Container _container;
+        private readonly IDistributedCache _cache;
 
-        public EmployeeRepository(CosmosClient cosmosClient, string databaseName, string containerName)
+        public EmployeeRepository(CosmosClient cosmosClient, string databaseName, string containerName, IDistributedCache cache)
         {
             _container = cosmosClient.GetContainer(databaseName, containerName);
+            _cache = cache;
         }
 
         public async Task CreateEmployeeAsync(Employee employee)
         {
             await _container.CreateItemAsync(employee, new PartitionKey(employee.Id));
+            // Invalidate the GetAllEmployees cache when a new employee is created
+            await _cache.RemoveAsync("all_employees");
         }
 
         public async Task DeleteEmployeeAsync(string id)
         {
             await _container.DeleteItemAsync<Employee>(id, new PartitionKey(id));
+            // Invalidate the cache for this specific employee
+            await _cache.RemoveAsync(id);
+            // Invalidate the GetAllEmployees cache when an employee is deleted
+            await _cache.RemoveAsync("all_employees");
         }
 
         public async Task<Employee> GetEmployeeAsync(string id)
         {
-            try
+            Employee employee = null;
+            string cachedEmployee = await _cache.GetStringAsync(id);
+
+            if (!string.IsNullOrEmpty(cachedEmployee))
             {
-                ItemResponse<Employee> response = await _container.ReadItemAsync<Employee>(id, new PartitionKey(id));
-                return response.Resource;
+                employee = JsonConvert.DeserializeObject<Employee>(cachedEmployee);
             }
-            catch (CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
+            else
             {
-                
-                return null;
+                try
+                {
+                    ItemResponse<Employee> response = await _container.ReadItemAsync<Employee>(id, new PartitionKey(id));
+                    employee = response.Resource;
+                    if (employee != null)
+                    {
+                        await _cache.SetStringAsync(id, JsonConvert.SerializeObject(employee), new DistributedCacheEntryOptions
+                        {
+                            AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(30) // Cache for 30 minutes
+                        });
+                    }
+                }
+                catch (CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    // Employee not found
+                    employee = null;
+                }
             }
+
+            return employee;
         }
 
         public async Task<IEnumerable<Employee>> GetAllEmployeesAsync()
         {
-            var query = _container.GetItemQueryIterator<Employee>(new QueryDefinition("SELECT * FROM c"));
-            List<Employee> results = new List<Employee>();
-            while (query.HasMoreResults)
+            string cachedEmployees = await _cache.GetStringAsync("all_employees");
+            IEnumerable<Employee> employees;
+
+            if (!string.IsNullOrEmpty(cachedEmployees))
             {
-                var response = await query.ReadNextAsync();
-                results.AddRange(response.ToList());
+                employees = JsonConvert.DeserializeObject<List<Employee>>(cachedEmployees);
             }
-            return results;
+            else
+            {
+                var query = _container.GetItemQueryIterator<Employee>(new QueryDefinition("SELECT * FROM c"));
+                List<Employee> results = new List<Employee>();
+                while (query.HasMoreResults)
+                {
+                    var response = await query.ReadNextAsync();
+                    results.AddRange(response.ToList());
+                }
+                employees = results;
+                if (employees.Any())
+                {
+                    await _cache.SetStringAsync("all_employees", JsonConvert.SerializeObject(employees), new DistributedCacheEntryOptions
+                    {
+                        AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(30) // Cache for 30 minutes
+                    });
+                }
+            }
+
+            return employees;
         }
 
         public async Task UpdateEmployeeAsync(Employee employee)
         {
             await _container.UpsertItemAsync(employee, new PartitionKey(employee.Id));
+            // Update the cache with the new employee details
+            await _cache.SetStringAsync(employee.Id, JsonConvert.SerializeObject(employee), new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(30) // Cache for 30 minutes
+            });
+            // Invalidate the GetAllEmployees cache when an employee is updated
+            await _cache.RemoveAsync("all_employees");
         }
     }
 }
